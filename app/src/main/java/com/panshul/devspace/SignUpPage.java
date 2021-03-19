2569d93096f34f86e449f81bbf3780de20bb1e8a
panshuljindal <60618820+panshuljindal@users.noreply.github.com>
2021-03-19 23:55:57 +0530

    Initialise page login and signup

--- app/src/main/java/com/panshul/devspace/SignUpPage.java
+++ app/src/main/java/com/panshul/devspace/SignUpPage.java
@@ -1,13 +1,14 @@
 package com.panshul.devspace;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class SignUpPage extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_sign_up_page);
     }
 }
